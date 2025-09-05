2ce6ee10a49181a33d3565b24768fb8dc9b72e36
Juan David <judavidpm@gmail.com>
2025-09-05 10:26:20 -0500

    commit inicial

---
+++ src/main/java/lenguaje/parcial1/desarrollo/Parcial1Desarrollo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package lenguaje.parcial1.desarrollo;
+
+/**
+ *
+ * @author judav
+ */
+public class Parcial1Desarrollo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
